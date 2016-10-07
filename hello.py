05a9c841669dee36c2ff485b7dcd196ed82731aa
KeithWilliamsGMIT <G00324844@gmit.ie>
2016-10-07 21:41:58 +0100

    Completed problem 5

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 	
-@app.route('/name/<name>')
-def name(name):
+#@app.route('/name/<name>')
+#def name(name):
+#	return "Your name is %s" % name
+
+@app.route('/name')
+def name():
+	name = request.args.get('name')
 	return "Your name is %s" % name
 
 if __name__ == "__main__":

1e0acdf29e3bfda99f16ade3af1cdf682967f70a
KEITH WILLIAMS - Student <G00324844@gmit.ie>
2016-10-18 14:46:33 +0100

    Fix commit

--- hello.py
+++ hello.py
@@ -10,21 +10,9 @@ def hello():
 #def name(name):
 #	return "Your name is %s" % name
 
-<<<<<<< HEAD
 @app.route('/name', methods=["GET", "POST"])
 def name():
 	name = request.args.get('name')
-=======
-#@app.route('/name')
-#def name():
-#	name = request.args.get('name')
-#	return "Your name is %s" % name
-
-@app.route('/name', methods=["GET", "POST"])
-def name():
-	print(request.form)
-	name = request.form["name"]
->>>>>>> d9d5ad1b3047464eb5ddb72c59f67b0cdd3c727a
 	return "Your name is %s" % name
 
 #@app.route('/name', methods=["GET", "POST"])
